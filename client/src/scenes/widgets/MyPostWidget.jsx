828423cc12085da580b1fc8ffc8d282ceaf7192a
Illia Lotfalian <122395038+illia1383@users.noreply.github.com>
2023-08-22 16:41:02 -0400

    updated api to cloud data base
    changed api calling

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -45,8 +45,8 @@ const MyPostWidget = ({ picturePath }) => {
       formData.append("picture", image);
       formData.append("picturePath", image.name);
     }
-
-    const response = await fetch(`http://localhost:3001/posts`, {
+ 
+    const response = await fetch(`https://severside-iih1.onrender.com/posts`, {
       method: "POST",
       headers: { Authorization: `Bearer ${token}` },
       body: formData,
