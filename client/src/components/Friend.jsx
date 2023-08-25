1d3c3868fe5c6451af43547b52b13efab7d4a9e0
illia <illialotfalian@gmail.com>
2023-08-25 17:01:10 -0400

    changed client, and removed non-functioning buttons

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -23,7 +23,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 
   const patchFriend = async () => {
     const response = await fetch(
-      `https://severside-iih1.onrender.com/users/${_id}/${friendId}`, //fixed error on this line 
+      `https://severside-iih1.onrender.com/users/${_id}/${friendId}`,    //fixed error on this line 
       {
         method: "PATCH",
         headers: {
--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -133,20 +133,6 @@ const MyPostWidget = ({ picturePath }) => {
 
         {isNonMobileScreens ? (
           <>
-            <FlexBetween gap="0.25rem">
-              <GifBoxOutlined sx={{ color: mediumMain }} />
-              <Typography color={mediumMain}>Clip</Typography>
-            </FlexBetween>
-
-            <FlexBetween gap="0.25rem">
-              <AttachFileOutlined sx={{ color: mediumMain }} />
-              <Typography color={mediumMain}>Attachment</Typography>
-            </FlexBetween>
-
-            <FlexBetween gap="0.25rem">
-              <MicOutlined sx={{ color: mediumMain }} />
-              <Typography color={mediumMain}>Audio</Typography>
-            </FlexBetween>
           </>
         ) : (
           <FlexBetween gap="0.25rem">
